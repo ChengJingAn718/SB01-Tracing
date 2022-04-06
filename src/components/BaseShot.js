cfd5de54cf6ff864945c19eb65f78562be0af8a1
superdeveloper718 <96727045+superdeveloper718@users.noreply.github.com>
2022-04-06 03:30:48 -0800

    Update BaseShot.js

--- src/components/BaseShot.js
+++ src/components/BaseShot.js
@@ -16,7 +16,7 @@ let backAudio = loadSound('bMusic', true)
 backAudio.loop = true;
 backAudio.volume = .1;
 
-const currentSceneNumber = 6;
+const currentSceneNumber = 7;
 
 // let primaryAudio = loadSound('SB03_01')
 // let titleAudio = loadSound('SB03_02')
